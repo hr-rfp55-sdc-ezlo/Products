417f2eb379c9d8387fff4d48d27ee903d37d3d1f
huongtran1993 <huongtran1993@gmail.com>
2021-09-30 12:00:24 -0700

    established connection to db

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,18 @@
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg')
 
-const connection = new Pool();
+const connection = new Pool({
+  user: 'huongnguyen',
+  host: 'localhost',
+  database: 'sdcdb',
+  port: 5432,
+});
+
+connection.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log('Error connecting to db: ', err);
+  } else {
+    console.log('Connected to db!')
+  }
+})
 
 module.exports = connection;
