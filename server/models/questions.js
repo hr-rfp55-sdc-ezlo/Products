d85e7bd816cdece6b85c428ef9d776de8df9a2c9
huongtran1993 <huongtran1993@gmail.com>
2021-10-01 00:46:09 -0700

    working on get questions query

--- server/models/questions.js
+++ server/models/questions.js
@@ -5,12 +5,8 @@ module.exports = {
   fetch: function (productId, callback) {
     // fetch all questions for a given product_id
     var query = {
-      // text: 'SELECT questions.question_id, question_body, question_date, asker_name, question_helpfulness, questions.reported, ARRAY_AGG (row_to_json(answers_short)) answers FROM questions INNER JOIN (SELECT question_id, id, body, date, answerer_name, helpfulness FROM answers) answers_short ON questions.question_id = answers_short.question_id WHERE product_id = $1 AND questions.reported = FALSE GROUP BY questions.question_id, question_body, question_date, asker_name, question_helpfulness, questions.reported',
-      // text: 'SELECT questions.question_id, question_body, question_date, asker_name, question_helpfulness, questions.reported, ARRAY_AGG (row_to_json(ap)) answers FROM questions LEFT OUTER JOIN (SELECT answers.question_id, answers.id, body, date, answerer_name, helpfulness, ARRAY_AGG (row_to_json(photos)) photos FROM answers LEFT OUTER JOIN photos ON answers.id = photos.answer_id GROUP BY answers.question_id, answers.id, body, date, answerer_name, helpfulness) ap ON questions.question_id = ap.question_id WHERE product_id = $1 AND questions.reported = FALSE GROUP BY questions.question_id, question_body, question_date, asker_name, question_helpfulness, questions.reported',
-      // text: 'SELECT questions.question_id, question_body, question_date, asker_name, question_helpfulness, questions.reported, ARRAY_AGG (row_to_json(c)) (SELECT * FROM questions LEFT OUTER JOIN answers ON questions.question_id = answers.question_id LEFT OUTER JOIN photos ON answers.id = photos.answer_id WHERE product_id = 1 AND questions.reported = FALSE)'
-      /*
-      text: `
-      SELECT q.question_id, question_body, question_date, asker_name, question_helpfulness, reported
+      /* OPTION 1. Need to pass down the question_id to reduce query time
+      SELECT q.question_id, question_body, question_date, asker_name, question_helpfulness, reported, a.answers
       FROM questions q
       LEFT OUTER JOIN
         (SELECT question_id, json_object_agg(id, row_to_json(ap)::jsonb - 'question_id') answers
@@ -24,12 +20,26 @@ module.exports = {
             GROUP BY question_id
         ) a
       ON q.question_id = a.question_id
-      WHERE q.question_id = 1;
       WHERE product_id = 1 AND reported = FALSE;
-      `
+      text:"SELECT q.question_id, question_body, question_date, asker_name, question_helpfulness, reported, a.answers FROM questions q LEFT OUTER JOIN (SELECT question_id, json_object_agg(id, row_to_json(ap)::jsonb - 'question_id') answers FROM (SELECT question_id, answers.id, body, date, answerer_name, helpfulness, ARRAY_AGG (row_to_json(photos)::jsonb - 'answer_id') photos FROM answers LEFT OUTER JOIN photos ON answers.id = photos.answer_id GROUP BY question_id, answers.id, body, date, answerer_name, helpfulness) ap WHERE question_id = 4 GROUP BY question_id) a ON q.question_id = a.question_id WHERE product_id = 1 AND reported = FALSE",
       */
-      text:"SELECT question_id, json_object_agg(id, row_to_json(ap)::jsonb - 'question_id') answers FROM (SELECT question_id, answers.id, body, date, answerer_name, helpfulness, ARRAY_AGG (row_to_json(photos)::jsonb - 'answer_id') photos FROM answers LEFT OUTER JOIN photos ON answers.id = photos.answer_id GROUP BY question_id, answers.id, body, date, answerer_name, helpfulness) ap GROUP BY question_id LEFT OUTER JOIN (SELECT q.question_id, question_body, question_date, asker_name, question_helpfulness, reported FROM questions q",
-      // values: [productId],
+      /* OPTION 2: join 3 tables first then reformat the data. ISSUE: answer_id can be null for questions with no answer...
+      SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported, json_object_agg(answer_id, json_build_object('answer_id', answer_id, 'body', body, 'date', date, 'answerer_name', answerer_name, 'helpfulness', helpfulness, 'photos', photos)) answers
+      FROM
+      (SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported, answer_id, body, date, answerer_name, helpfulness, json_agg(json_build_object('id', id, 'url', url)) photos
+      FROM
+      (SELECT q.question_id, question_body, question_date, asker_name, question_helpfulness, q.reported, a.id answer_id, body, date, answerer_name, helpfulness, p.id, url
+      FROM questions q
+      LEFT OUTER JOIN answers a
+      ON q.question_id = a.question_id
+      LEFT OUTER JOIN photos p
+      ON a.id = p.answer_id
+      WHERE product_id =1 and q.reported = FALSE) t1
+      GROUP BY question_id, question_body, question_date, asker_name, question_helpfulness, reported, answer_id, body, date, answerer_name, helpfulness) t2
+      GROUP BY question_id, question_body, question_date, asker_name, question_helpfulness, reported
+      */
+      text:"SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported, json_agg(json_build_object('answer_id', answer_id, 'body', body, 'date', date, 'answerer_name', answerer_name, 'helpfulness', helpfulness, 'photos', photos)) answers FROM (SELECT question_id, question_body, question_date, asker_name, question_helpfulness, reported, answer_id, body, date, answerer_name, helpfulness, json_agg(json_build_object('id', id, 'url', url)) photos FROM (SELECT q.question_id, question_body, question_date, asker_name, question_helpfulness, q.reported, a.id answer_id, body, date, answerer_name, helpfulness, p.id, url FROM questions q LEFT OUTER JOIN answers a ON q.question_id = a.question_id LEFT OUTER JOIN photos p ON a.id = p.answer_id WHERE product_id = $1 and q.reported = FALSE) t1 GROUP BY question_id, question_body, question_date, asker_name, question_helpfulness, reported, answer_id, body, date, answerer_name, helpfulness) t2 GROUP BY question_id, question_body, question_date, asker_name, question_helpfulness, reported",
+      values: [productId],
     };
     db.query(query, (err, results) => {
       if (err) {
@@ -37,7 +47,7 @@ module.exports = {
       } else {
         callback(null, results.rows);
       }
-    });
+      });
   },
   create: function (params, callback) {
     // create a question for the given product_id
