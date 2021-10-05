aa835e58a01aa68ecb796a6cf437793a86ec9f5e
EC2 Default User <ec2-user@ip-172-31-27-195.us-west-1.compute.internal>
2021-10-05 03:37:28 +0000

    updated db connection & added static file for loader io verification

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,9 @@ const { Pool } = require('pg');
 
 const connection = new Pool({
   user: 'huongnguyen',
-  host: 'localhost',
+  host: 'ec2-13-52-252-73.us-west-1.compute.amazonaws.com',
   database: 'sdcdb',
+  password: 'sdc',
   port: 5432,
 });
 
